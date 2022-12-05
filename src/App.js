00588efb6eb9343a18dd9030aa7e8d546001761e
KovalevichPavel <kovalevichpasha1@gmail.com>
2022-12-05 19:47:58 +0300

    can create notes

--- src/App.js
+++ src/App.js
@@ -4,10 +4,18 @@ import Note from './components/Note';
 
 function App() {
   const [newTitle, setNewTitle] = useState('');
+  const [notes, setNotes] = useState([{title: 'first note', id: 0}]);
 
   const handleChange = (e) => {
-    console.log(newTitle.length);
     setNewTitle(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    const addNote = {title: newTitle, id: notes.length};
+    setNotes([...notes, addNote])
+    setTimeout(() => console.log(notes), 1);
+    setNewTitle('');
+    e.preventDefault();
   }
 
   return (
@@ -18,23 +26,22 @@ function App() {
       <hr className="mb-5"/>
 
       <div className='content'> 
-        <form className="d-flex mb-2" role="search">
+        <form className="d-flex mb-2" onSubmit={handleSubmit}>
           <input 
             className="form-control me-2"
             placeholder="write note title here"
             value={newTitle}
             onChange={handleChange}
           />
-          <button className="btn btn-outline-success"
+          <button 
+            className="btn btn-outline-success"
             type="submit">
             Create
           </button>
         </form>               
 
         <ul className="list-group">
-          <Note title='An item'/>
-          <Note title='A second item'/>
-          <Note title='A third item'/>          
+          {(notes).map((note) => <Note title={note.title} key={note.id}/>)}
         </ul>
 
       </div>      
--- src/components/Note.js
+++ src/components/Note.js
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function Note(props) {
     return (
-        <li className="list-group-item note">
+        <li className="list-group-item note" key={props.id}>
             <div>
-                <span>{props.title}</span>         
-                <small></small>             
+                <span>{props.title}</span>             
+                <small></small>   
             </div>            
 
             <button type="button" className="btn btn-danger btn-sm">&times;</button>
