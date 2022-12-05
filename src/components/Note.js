93392d3e1139a83adccdc0aeb18e91a9e0b44297
KovalevichPavel <kovalevichpasha1@gmail.com>
2022-12-05 20:03:42 +0300

    can delete notes

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 export default function Note(props) {
+    function removeNote(e) {
+        e.target.parentNode.remove();
+    }
+
     return (
         <li className="list-group-item note" key={props.id}>
             <div>
@@ -8,7 +12,7 @@ export default function Note(props) {
                 <small></small>   
             </div>            
 
-            <button type="button" className="btn btn-danger btn-sm">&times;</button>
+            <button type="button" className="btn btn-danger btn-sm" onClick={removeNote}>&times;</button>
         </li>
     )
 }
