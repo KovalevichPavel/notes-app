c0d28b203040308acd035446b2faa0e3c5cf314f
KovalevichPavel <kovalevichpasha1@gmail.com>
2022-12-06 03:03:54 +0300

    can create, modify, delete, add tags and almost search by tags

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, {useState} from 'react';
-import Header from './components/Header';
 import Note from './components/Note';
 
 function App() {
   const [newTitle, setNewTitle] = useState('');
   const [notes, setNotes] = useState([{title: 'first note', id: 0}]);
+  const [tags, setTags] = useState('');
+
+  function handleChangeTags(e) {
+    setTags(e.target.value);
+  }
 
   const handleChange = (e) => {
     setNewTitle(e.target.value);
   }
 
   const handleSubmit = (e) => {
+    if (newTitle.length === 0 || newTitle.length >= 50) {
+      e.preventDefault();
+      return;
+    }
+
     const addNote = {title: newTitle, id: notes.length};
-    setNotes([...notes, addNote])
-    setTimeout(() => console.log(notes), 1);
+    setNotes([...notes, addNote]);
+
     setNewTitle('');
     e.preventDefault();
   }
@@ -21,7 +30,16 @@ function App() {
   return (
     
     <div className="container">
-      <Header />
+      <div className="header">
+        <h1>Note APP</h1>
+        <form className="d-flex" role="search">
+          <input 
+            className="form-control me-2"
+            placeholder="Search in tags"
+            onChange={handleChangeTags}
+        />
+        </form>
+      </div>
 
       <hr className="mb-5"/>
 
@@ -41,7 +59,10 @@ function App() {
         </form>               
 
         <ul className="list-group">
-          {(notes).map((note) => <Note title={note.title} key={note.id}/>)}
+          {(notes).map((note) => {
+
+            return <Note title={note.title} key={note.id} tags={tags}/>
+          })}
         </ul>
 
       </div>      
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,18 +1,63 @@
-import React from "react";
+import React, {useState, useEffect, useRef} from "react";
 
-export default function Note(props) {
+export default function Note(props) {    
+    const textareaEl = useRef(null);
+    const smallEl = useRef(null);
+    const [description, setDescription] = useState(''); 
+    
+    // useEffect(() => {
+    //     let tagsArrOfSearch = props.tags.split(' ');
+    //     let tagsArrOfNote = smallEl.current.innerHTML.split(' ');
+    //     let hasMatchingTags = false;
+    //     for (let tagOfNote of tagsArrOfNote) {
+    //         if (tagsArrOfSearch.includes(tagOfNote)) hasMatchingTags = true;
+    //     }
+    //     if (hasMatchingTags === false) return null;
+    // },)
+
     function removeNote(e) {
-        e.target.parentNode.remove();
+        e.target.parentNode.parentNode.remove();
+    }
+
+    function showDescription() {
+        textareaEl.current.classList.toggle('hidden');
+    }
+
+    function handleChange(e) {
+        setDescription(e.target.value);
+
+        let words = e.target.value.split(' ');
+        let arrOfTags = [];
+        for (let word of words) {
+            if (word[0] === '#') {
+                arrOfTags.push(word);
+            }
+        }
+        smallEl.current.innerHTML = arrOfTags.join(' ');
     }
 
     return (
-        <li className="list-group-item note" key={props.id}>
-            <div>
-                <span>{props.title}</span>             
-                <small></small>   
-            </div>            
+        <li className="list-group-item note" onClick={showDescription} key={props.id}>
+            <div className="note-title">
+                <div>
+                    <span>{props.title}</span>             
+                    <small ref={smallEl}></small> {/* this is for displaying tags */}
+                </div>            
 
-            <button type="button" className="btn btn-danger btn-sm" onClick={removeNote}>&times;</button>
+                <button type="button"
+                    className="btn btn-danger btn-sm"
+                    onClick={removeNote}>
+                    &times;
+                </button>
+            </div>
+
+            <textarea 
+                value={description}
+                onChange={handleChange}
+                className="hidden mt-4"
+                onClick={() => {textareaEl.current.classList.toggle('hidden')}}
+                ref={textareaEl}
+            />
         </li>
     )
 }
